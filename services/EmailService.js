81f2507903b2156dfdf45e23d3f73daa8942d119
Surfsol <surfsol@protonamil.com>
2021-04-17 15:51:30 -0300

    add emails verifyAccount and account cancellation

--- services/EmailService.js
+++ services/EmailService.js
@@ -12,42 +12,102 @@ let transporter = nodemailer.createTransport({
 const sendResetPasswordEmail = (user, code, url) => {
   transporter.sendMail(
     {
-      from: "Sauti Trade Insights <tradeinsights.sautiafrica@gmail.com>",
+      from: "Sauti Trade Insights <rterrydeve@gmail.com>",
       to: `${user.email}`,
       subject: "Code Verification - NO REPLY",
       text: "For clients with plaintext support only",
       html: `<p>Hello ${user.email}, <br /> Your verifcation code to reset your password is <b>${code}</b>. <br /> Please visit <a href="${url}">Password Reset Verification Link</a> to reset your password.  </p>`
     },
-    (err, info) => {
-      console.log(info.envelope);
-      console.log(info.messageId);
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
     }
   );
 };
 
 const sendAccountCancellation = (email, cancelDate) => {
+  const link =
+    "https://docs.google.com/forms/d/e/1FAIpQLSfeKJkwkHPvNO4fJo6OJfKCNSKnWH7UhraJ8bpFdRT4loutfw/viewform?usp=sf_link";
   transporter.sendMail(
     {
-      from: "Sauti Trade Insights <tradeinsights.sautiafrica@gmail.com>",
+      from: "Sauti Trade Insights <rterrydeve@gmail.com>",
       to: `${email}`,
       subject: "Subscription Cancellation - NO REPLY",
       text: "For clients with plaintext support only",
-      html: `<p>Hello ${email}, <br /> You have cancelled your subscription, your Premium Access will expire on ${cancelDate}.</p>`
+      html: `
+      <html>
+      <head>
+      </head>
+      <body style="padding: 20px;background-color: #f3f3f3;">
+        <center>
+          <div style="width: 300px;border: 15px solid #025467;padding: 50px;margin: 20px;">
+            <h1 style="font-family: &quot;Roboto&quot;, sans-serif;font-size: 24px;color: #e84a34;">Two Week Trial Email Verification</h1>
+            <p style="font-family: &quot;Nunito&quot;, sans-serif
+            font-size: 14px;">Hi ${email}</name></p>
+            <p style="font-family: &quot;Nunito&quot;, sans-serif
+            font-size: 14px;">Thank you for using Sauti Trade Insights. We hope you found the information you were looking for.</p>
+            <p style="font-family: &quot;Nunito&quot;, sans-serif
+            font-size: 14px;">As per your request, your subscription has been cancelled. The good news is that your account will be active until ${cancelDate} and you can still access Sauti Trade Insights in the meantime.</end-date></p>
+            <p style="font-family: &quot;Nunito&quot;, sans-serif
+            font-size: 14px;">We’d like to learn the reason behind your cancellation so we can better serve our users (and hopefully you!) in the future .</p>
+      
+            <button  style="color: #f3f3f3;background-color: #e84a34;font-family: &quot;Roboto&quot;, sans-serif;font-size: 20px;border-color: transparent;border-radius: 4px;text-align: center;cursor: pointer;"><a href="${link}" style="text-decoration:none">Feedback Survey</a></button>
+      
+            <p style="font-family: &quot;Nunito&quot;, sans-serif
+            font-size: 14px;">Thanks, The Sauti Team</p>
+          </div>
+        </center>
+      </body>
+      </html>
+     `
     },
     (err, info) => {
-      console.log(info.envelope);
-      console.log(info.messageId);
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
     }
   );
 };
 
 const sendVerifyAccount = (user, url) => {
   transporter.sendMail({
-    from: "Sauti Trade Insights <tradeinsights.sautiafrica@gmail.com>",
+    from: "Sauti Trade Insights <rterrydeve@gmail.com>",
     to: `${user.email}`,
     subject: "Email Verification - NO REPLY",
     text: "For clients with plaintext support only",
-    html: `<p>Hello ${user.email}, <br /> Please visit <a href="${url}">Email Verification Link</a> to verify your email.</p>`
+    html: `
+    <html>
+    <head>
+    </head>
+    <body style="padding: 20px;background-color: #f3f3f3;">
+    <center>
+      <div>
+        <h1 style="font-family: &quot;Roboto&quot;, sans-serif;font-size: 24px;color: #e84a34;">Two Week Trial Email Verification</h1>
+        <p style="font-family: &quot;Nunito&quot;, sans-serif
+          font-size: 14px;">Hi ${user.email}</p>
+        <p style="font-family: &quot;Nunito&quot;, sans-serif
+          font-size: 14px;">Thank you for signing up for Sauti Trade Insights</p>
+        <p style="font-family: &quot;Nunito&quot;, sans-serif
+          font-size: 14px;">Verify your email address to start your two week trial access. Click on the link below to verify that ${user.email} belongs to you.</p>
+    ​
+        <button style="color: #f3f3f3;background-color: #e84a34;font-family: &quot;Roboto&quot;, sans-serif;font-size: 20px;border-color: transparent;border-radius: 4px;text-align: center;cursor: pointer;"><a href="${url}" style="text-decoration:none">Verify Email</a></button>
+    ​
+        <p style="font-family: &quot;Nunito&quot;, sans-serif
+          font-size: 14px;">Thanks, The Sauti Team</p>
+    ​
+        <p style="font-size: 10pt;font-family: &quot;Nunito&quot;, sans-serif
+          font-size: 14px;">*If you didn't recently attempt to create a new account with this email address, you can safely disregard this email.</p>
+      </div>
+    </center>
+    ​
+    </body>
+    </html>
+    `
   });
 };
 
@@ -68,7 +128,7 @@ const sendSuccess = (user, type) => {
       break;
   }
   transporter.sendMail({
-    from: "Sauti Trade Insights <tradeinsights.sautiafrica@gmail.com>",
+    from: "Sauti Trade Insights <rterrydeve@gmail.com>",
     to: user.email,
     subject: `${subject}`,
     text: "For clients with plaintext support only",
