cd11dd11298c8e0f02437388362ecf21bcbdacaa
Surfsol <surfsol@protonamil.com>
2021-05-11 21:33:40 -0300

    update tradersDataUpdate

--- models/tradersDataUpdate.js
+++ models/tradersDataUpdate.js
@@ -2,9 +2,11 @@ const db = require("./model");
 const data = require("./tradersData.js")
 
 try {
-  console.log("\n** TRADERS TABLE **\n", Date(Date.now().toString()))
+  db.truncateTable("traders");
+  console.log("\n** TRADERS TABLE **\n", Date(Date.now().toString()));
   const array = data.array
-  db.batchInsert("traders", array);
+  db.batchInsert("traders", array)
+  console.log("\n** TRADERS TABLE **\n", Date(Date.now().toString()))
 } catch {
   console.log("Failed to update Traders table");
 }
